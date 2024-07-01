f95642cd34777b2c87fb4a4d72183631e1eb3b84
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-07-01 22:52:49 +0530

    Task 1 Git Initialization and Project Folder and Code Structuring

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
